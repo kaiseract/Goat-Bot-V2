18f89ba7dba5ca7b3b66d0398a1cbdfb7d420b42
mamun10fxa <mamun10fxa@gmail.com>
2024-08-27 15:28:20 +0600

    Add files via upload

---
+++ scripts/cmds/owner.js
@@ -0,0 +1,67 @@
+const { GoatWrapper } = require('fca-liane-utils');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+	config: {
+		name: "owner",
+		author: "Tokodori",
+		role: 0,
+		shortDescription: " ",
+		longDescription: "",
+		category: "admin",
+		guide: "{pn}"
+	},
+
+	onStart: async function ({ api, event }) {
+		try {
+			const ownerInfo = {
+				name: 'Abdullah Al Mamun',   
+				address: 'Chapainawabganjawabganj,Rajshahi,Bangladesh',
+				facebook: 'https://facebook.com/61550553240301',
+				whatsapp: 'https://wa.me/message/Z7OCZN2RKQZFP1',
+     telegram: 'https://t.me/user4041',
+     email: 'mamun10fxa@proton.me' 
+			};
+
+			const bold = 'https://i.imgur.com/Kyo3frH.mp4';
+			const tmpFolderPath = path.join(__dirname, 'tmp');
+
+			if (!fs.existsSync(tmpFolderPath)) {
+				fs.mkdirSync(tmpFolderPath);
+			}
+
+			const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
+			const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
+
+			fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
+
+			const response = `
+◈ 𝖮𝖶𝖭𝖤𝖱 𝖨𝖭𝖥𝖮𝖱𝖬𝖠𝖳𝖨𝖮𝖭:\n
+
+Name: ${ownerInfo.name}
+Address: ${ownerInfo.address}
+Fb: ${ownerInfo.fb}
+Whatsapp: ${ownerInfo.whatsapp}
+Telegram: ${ownerInfo.telegram}
+E-mail: ${ownerInfo.email}
+			`;
+
+			await api.sendMessage({
+				body: response,
+				attachment: fs.createReadStream(videoPath)
+			}, event.threadID, event.messageID);
+
+			fs.unlinkSync(videoPath);
+
+			api.setMessageReaction('🌊', event.messageID, (err) => {}, true);
+		} catch (error) {
+			console.error('Error in ownerinfo command:', error);
+			return api.sendMessage('An error occurred while processing the command.', event.threadID);
+		}
+	}
+};
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
