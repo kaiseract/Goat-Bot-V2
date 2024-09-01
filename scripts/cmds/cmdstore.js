851956cf3009dc61138990ad1368865c5f49fd39
mamun10fxa <mamun10fxa@gmail.com>
2024-09-01 14:28:21 +0600

    Update cmdstore.js

--- scripts/cmds/cmdstore.js
+++ scripts/cmds/cmdstore.js
@@ -1,4 +1,59 @@
-=> {
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: 'cmdstore',
+    version: '1.0',
+    author: 'Vex_Kshitiz',
+    role: 0,
+    shortDescription: 'store of cmds',
+    longDescription: 'store of cmds all  made by kshitiz',
+    category: 'utility',
+    guide: {
+      en: 'To view commands: {p}cmdstore\nTo paginate: {p}cmdstore {page}\nTo search: {p}cmdstore {search}'
+    }
+  },
+
+  onStart: async function ({ api, event, args, message }) {
+    try {
+      let page = 1;
+      let searchQuery = "";
+
+      if (args.length === 1 && !isNaN(parseInt(args[0]))) {
+        page = parseInt(args[0]);
+      } else if (args.length === 1 && typeof args[0] === 'string') {
+        searchQuery = args[0];
+      } else if (args.length === 2 && args[0] === 'search' && typeof args[1] === 'string') {
+        searchQuery = args[1];
+      }
+
+      const response = await axios.get("https://cmd-store.vercel.app/kshitiz");
+      const commands = response.data;
+
+      let filteredCommands = commands;
+      if (searchQuery) {
+        filteredCommands = commands.filter(cmd => cmd.cmdName.toLowerCase().includes(searchQuery.toLowerCase()));
+      }
+
+      const startIndex = (page - 1) * 10;
+      const endIndex = page * 10;
+      const paginatedCommands = filteredCommands.slice(startIndex, endIndex);
+
+      let replyMessage = "";
+      paginatedCommands.forEach(cmd => {
+        replyMessage += `
+        ð—œð——:${cmd.id}
+        ð—–ð— ð——:${cmd.cmdName}
+        ð—–ð—¢ð——ð—˜:${cmd.codeLink}
+        ð—œð—¡ð—™ð—¢:${cmd.description}
+      ----------------------------------------------`;
+      });
+
+      if (replyMessage === "") {
+        replyMessage = "No commands found.";
+      }
+
+      message.reply(replyMessage, (err, info) => {
         global.GoatBot.onReply.set(info.messageID, {
           commandName: "cmdstore",
           messageID: info.messageID,
